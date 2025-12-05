5d6fc232aa38c06f918f3158db08ccd84a9b6a09
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-12-05 18:43:53 +0200

    Day04 part 2

--- python/src/aoc2025/days/day04.py
+++ python/src/aoc2025/days/day04.py
@@ -31,7 +31,32 @@ def part_one(input_text: str) -> int | None:
 
 def part_two(input_text: str) -> int | None:
     """Solve part two."""
-    lines = input_text.strip().split("\n")
+    grid = Grid.from_string(input_text.strip())
 
-    # TODO: Implement solution
-    return None
+    nice_neighbours = 0
+    old_val = nice_neighbours
+
+    while True:
+        to_remove = []
+        for (x, y), c in grid.iter():
+            if c == "@" and (
+                len(
+                    list(
+                        filter(
+                            lambda e: grid.get(e[0], e[1]) == "@",
+                            grid.neighbors_diagonal(x, y),
+                        )
+                    )
+                )
+                < 4
+            ):
+                nice_neighbours += 1
+                to_remove.append((x, y))
+
+        for x, y in to_remove:
+            grid.set(x, y, ".")
+
+        if len(to_remove) == 0:
+            break
+
+    return nice_neighbours
