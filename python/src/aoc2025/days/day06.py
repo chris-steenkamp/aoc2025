f941ff7e1a9100756061db70b745d3616d35c67b
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-12-06 11:19:47 +0200

    Day06 part 2

--- python/src/aoc2025/days/day06.py
+++ python/src/aoc2025/days/day06.py
@@ -5,6 +5,7 @@ https://adventofcode.com/2025/day/6
 
 from math import prod
 
+from aoc2025.utils.grid import Grid
 from aoc2025.utils.parsing import parse_unsigned
 
 operations = {"*": prod, "+": sum}
@@ -26,7 +27,29 @@ def part_one(input_text: str) -> int | None:
 
 def part_two(input_text: str) -> int | None:
     """Solve part two."""
-    lines = input_text.strip().split("\n")
+    lines = input_text.split("\n")
 
-    # TODO: Implement solution
-    return None
+    grid = Grid(lines[:-2]).rotate_left()
+
+    inputs = ["".join(grid.row(y)).strip() for y in range(grid.height)]
+    opcodes = list(reversed([opcode for opcode in lines[-2].split()]))
+
+    opcode_index = 0
+    opcode = opcodes[opcode_index]
+    subtotal = 0 if opcode == "+" else 1
+    total = 0
+
+    for val in inputs:
+        if val:
+            subtotal = operations[opcode]([subtotal, int(val)])
+        else:
+            # we've found a blank which means we start processing the next operation
+            opcode_index += 1
+            total += subtotal
+            opcode = opcodes[opcode_index]
+            subtotal = 0 if opcode == "+" else 1
+
+    # finally add the last calculated subtotal back to the total
+    total += subtotal
+
+    return total
