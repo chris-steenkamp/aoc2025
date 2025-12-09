d8a90e1d0c72f3cf3a3e7d6f5ffdb26c405ec23a
Chris Steenkamp <cdsteenkamp@gmail.com>
2025-12-09 19:04:25 +0200

    Rewrite part 1 to use similar logic to part 2 (remove the graph traversal).

--- python/src/aoc2025/days/day08.py
+++ python/src/aoc2025/days/day08.py
@@ -4,7 +4,7 @@ https://adventofcode.com/2025/day/8
 """
 
 import heapq
-from collections import defaultdict, deque
+from collections import Counter, defaultdict
 from math import prod
 
 from aoc2025.utils.direction import Point3, euclidean_difference
@@ -23,31 +23,42 @@ def part_one(input_text: str) -> int | None:
             if p != p2:
                 heapq.heappush(distances, (euclidean_difference(p, p2), p, p2))
 
-    circuits = defaultdict(list)
+    connections = defaultdict(int)
+    circuits = defaultdict(set)
+    circuit_num = 0
+
     for i in range(size):
         _, p1, p2 = heapq.heappop(distances)
-        circuits[p1].append(p2)
-        circuits[p2].append(p1)
+        p1_circuit = connections[p1]
+        p2_circuit = connections[p2]
 
-    visited = []
-    keys = list(circuits.keys())
+        if p1_circuit == p2_circuit == 0:
+            circuit_num += 1
+            new_circuit = circuit_num
+            circuits[new_circuit].add(p1)
+            circuits[new_circuit].add(p2)
+        elif p1_circuit == 0:
+            new_circuit = p2_circuit
+            circuits[new_circuit].add(p1)
+        elif p2_circuit == 0:
+            new_circuit = p1_circuit
+            circuits[new_circuit].add(p2)
+        elif p1_circuit != p2_circuit:
+            new_circuit = p1_circuit
+            for c in circuits[p2_circuit]:
+                connections[c] = new_circuit
 
-    for i in range(len(keys)):
-        visited.append(set())
-        start = keys[i]
+            circuits[new_circuit] |= circuits[p2_circuit]
+            del circuits[p2_circuit]
+        else:
+            continue
 
-        v = deque([start])
-        while len(v):
-            current = v.popleft()
-            visited[i].add(current)
-            for n in circuits[current]:
-                if n not in visited[i]:
-                    v.append(n)
+        connections[p1] = new_circuit
+        connections[p2] = new_circuit
 
-    circuits = sorted(
-        [len(s) for s in set(frozenset(v) for v in visited)], reverse=True
-    )
-    return prod(circuits[:3])
+    totals = Counter(connections.values())
+
+    return prod(map(lambda x: x[1], totals.most_common(3)))
 
 
 def part_two(input_text: str) -> int | None:
@@ -63,7 +74,7 @@ def part_two(input_text: str) -> int | None:
             if p1 != p2:
                 heapq.heappush(distances, (euclidean_difference(p1, p2), p1, p2))
 
-    while len(junction_boxes) > 0 and len(distances) > 0:
+    while len(junction_boxes) > 0:
         _, p1, p2 = heapq.heappop(distances)
 
         total = p1[0] * p2[0]
